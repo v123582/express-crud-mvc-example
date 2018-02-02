8512b944e08113348bde1df839dc95caf1f5fea3
v123582 <v123582@gmail.com>
2018-02-03 00:31:50 +0800

    sequelize db:seed

---
+++ seeders/20180202162904-my-seed-file.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Tasks', [{
+        title: '測試 A',
+        createdAt : new Date(),
+        updatedAt : new Date(),
+      }, {
+        title: '測試 B',
+        createdAt : new Date(),
+        updatedAt : new Date(),
+      }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
